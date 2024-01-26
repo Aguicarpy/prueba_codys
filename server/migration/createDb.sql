c5152550d920616b9ab35784bacacf0c2340178d
Aguicarpy <aguicarpy@gmail.com>
2024-01-26 05:00:48 -0300

    edit createDbScript

--- server/migration/createDb.sql
+++ server/migration/createDb.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS form_codys;
 USE form_codys;
 
-CREATE TABLE IF NOT EXISTS formulario (
+CREATE TABLE IF NOT EXISTS form (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  profile VARCHAR(255)
+  profile VARCHAR(255),
   fullname VARCHAR(255),
   email VARCHAR(255),
   phone VARCHAR(255),
-  birthday DATE,
+  birthday DATE
 );
--- server/migration/createDbScript.js
+++ server/migration/createDbScript.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 
+//Script para crear su base de datos y la tabla correspondiente al formulario
 try {
   execSync('"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe" -h localhost -u root -p123456 < createDb.sql');
   console.log('Base de datos y tabla creadas exitosamente.');
